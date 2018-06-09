044c70c3660f0d2f2a1f819886caba3ca2cfedd7
Swapratim <Swapratim@users.noreply.github.com>
2018-06-09 06:21:10 +0200

    Add files via upload

--- popular_courses.py
+++ popular_courses.py
@@ -1,2 +1,345 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+from future import standard_library
+import requests
+standard_library.install_aliases()
+import urllib.request, urllib.parse, urllib.error
+import json
+import os
+import sys
+
+from flask import Flask
+from flask import request, render_template
+from flask import make_response
+
+#************************************************************************************#
+#                                                                                    #
+#   POPULAR COURSE CATEGORY - SHOWS FOUR MAJOR CATEGORIES EACH WITH 10 ITEMS         #
+#                                                                                    #
+#************************************************************************************#
+def popularcourses():
+    print ("I'm in the popularcourses  method")
+    res = {
+        "speech": "Taking up popular courses is a quick way to accelerate your career",
+        "displayText": "Taking up popular courses is a quick way to accelerate your career",
+        "data" : {
+        "facebook" : [
+               {
+                "text": "Taking up popular courses is a quick way to accelerate your career."
+               },
+               {
+                    "sender_action": "typing_on"
+               },
+               {
+                 "attachment" : {
+                   "type" : "template",
+                     "payload" : {
+                      "template_type" : "generic",
+                       "elements" : [ 
+                                 {
+                                   "title" : "Computer Science",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Computer Science"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 }, 
+                                 {
+                                   "title" : "Data & Statistics",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Data & Statistics"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Business Management",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Business Management"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Language",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Language"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Economics & Finance",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Economics & Finance"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Engineering",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Engineering"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Humanities",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Humanities"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Life Sciences",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Life Sciences"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 } 
+                           ]
+                       } 
+                   }
+                },
+                {
+                  "text": "Get me to previous step",
+                  "quick_replies": [
+                 {
+                  "content_type": "text",
+                  "title": "Back",
+                  "payload": "BackToHome",
+                  "image_url": "https://maxcdn.icons8.com/Share/icon/Arrows/reply_all_arrow_filled1600.png"
+                  }
+                  ]
+                }
+             ]
+          } 
+       };
+    print (res)
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+#************************************************************************************#
+#                                                                                    #
+#   PROFESSIONAL CERTIFICATES - THIS CONTAINS 10 COURSE ITEMS                        #
+#                                                                                    #
+#************************************************************************************#
+def computerscience():
+    print ("I'm in the computerscience method")
+    res = {
+        "speech": "Select the best Computer Science courses from my collection",
+        "displayText": "Select the best Computer Science courses from my collection",
+        "data" : {
+        "facebook" : [
+               {
+                "text": "Select the best Computer Science courses from my collection"
+               },
+              {
+                    "sender_action": "typing_on"
+              },
+              {
+                 "attachment" : {
+                   "type" : "template",
+                     "payload" : {
+                      "template_type" : "generic",
+                       "elements" : [ 
+                                 {
+                                   "title" : "Robotics: Vision Intelligence and Machine Learning",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "http://tidd.ly/a06e249a",
+                                        "title": "Enroll for Free"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 }, 
+                                 {
+                                   "title" : "Introduction to Computational Thinking",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "http://tidd.ly/d10bf051",
+                                        "title": "Enroll for Free"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "UX Design: From Concept to Wireframe",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "http://tidd.ly/6317fe63",
+                                        "title": "Enroll for Free"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Introduction to Java Programming",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "http://tidd.ly/15632da2",
+                                        "title": "Enroll for Free"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "AWS Developer: Deploying on AWS",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "http://tidd.ly/f8f99b57",
+                                        "title": "Enroll for Free"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "IT Support: Cloud Fundamentals",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "http://tidd.ly/97b56c0c",
+                                        "title": "Enroll for Free"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Principles of Machine Learning",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "http://tidd.ly/e285776c",
+                                        "title": "Enroll for Free"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Introduction to OpenStack",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "http://tidd.ly/e5399528",
+                                        "title": "Enroll for Free"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Infrastructure as a code",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "http://tidd.ly/ddc74bca",
+                                        "title": "Enroll for Free"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "DevOps for Mobile Apps",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "web_url",
+                                        "url": "http://tidd.ly/fc77e59",
+                                        "title": "Enroll for Free"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 }
+                           ]
+                       } 
+                   }
+                },
+                {
+                  "text": "Get me to previous step",
+                  "quick_replies": [
+                 {
+                  "content_type": "text",
+                  "title": "Back",
+                  "payload": "BackToProfessionalCourses",
+                  "image_url": "https://maxcdn.icons8.com/Share/icon/Arrows/reply_all_arrow_filled1600.png"
+                  }
+                  ]
+                }
+             ]
+          } 
+       };
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
