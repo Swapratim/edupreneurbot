080741a07cc3283893044357115595defb51a20b
Swapratim <Swapratim@users.noreply.github.com>
2018-06-09 06:19:16 +0200

    Add files via upload

---
+++ popularCourses/computer_science.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future import standard_library
+import requests
+standard_library.install_aliases()
+import urllib.request, urllib.parse, urllib.error
+import json
+import os
+import sys
+
+from flask import Flask
+from flask import request, render_template
+from flask import make_response
+
+#************************************************************************************#
+#                                                                                    #
+#   POPULAR COURSE CATEGORY - SHOWS FOUR MAJOR CATEGORIES EACH WITH 10 ITEMS         #
+#                                                                                    #
+#************************************************************************************#
+def computerscience():
+    print ("I'm in the popularcourses  method")
+    res = {
+        "speech": "Taking up popular courses is a quick way to accelerate your career",
+        "displayText": "Taking up popular courses is a quick way to accelerate your career",
+        "data" : {
+        "facebook" : [
+               {
+                "text": "Taking up popular courses is a quick way to accelerate your career."
+               },
+               {
+                    "sender_action": "typing_on"
+               },
+               {
+                 "attachment" : {
+                   "type" : "template",
+                     "payload" : {
+                      "template_type" : "generic",
+                       "elements" : [ 
+                                 {
+                                   "title" : "Computer Science",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Computer Science"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 }, 
+                                 {
+                                   "title" : "Data & Statistics",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Data & Statistics"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Business Management",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Business Management"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Language",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Language"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Economics & Finance",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Economics & Finance"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Engineering",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Engineering"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Humanities",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Humanities"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 },
+                                 {
+                                   "title" : "Life Sciences",
+                                   "image_url" : "",
+                                   "subtitle" : "",
+                                   "buttons": [{
+                                        "type": "postback",
+                                        "title": "Explore Courses",
+                                        "payload": "Life Sciences"
+                                    },
+                                    {
+                                        "type": "element_share"
+                                    }]
+                                 } 
+                           ]
+                       } 
+                   }
+                },
+                {
+                  "text": "Get me to previous step",
+                  "quick_replies": [
+                 {
+                  "content_type": "text",
+                  "title": "Back",
+                  "payload": "BackToHome",
+                  "image_url": "https://maxcdn.icons8.com/Share/icon/Arrows/reply_all_arrow_filled1600.png"
+                  }
+                  ]
+                }
+             ]
+          } 
+       };
+    print (res)
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
